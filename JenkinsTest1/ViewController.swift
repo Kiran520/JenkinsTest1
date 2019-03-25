f857e1263b1e9028f70116962eecda0654d246dc
Unknown <kiranv520@gmail.com>
2019-03-25 22:26:44 +0100

    First commit

---
+++ JenkinsTest1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JenkinsTest1
+//
+//  Created by Kiran on 3/25/19.
+//  Copyright © 2019 Kiran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
